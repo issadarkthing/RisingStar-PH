ed0fdc02add7671c4ac000af6be3c4fd3f883869
raziman <raziman.terra@gmail.com>
2021-09-07 14:12:25 +0800

    setup database

---
+++ src/database/User.ts
@@ -0,0 +1,22 @@
+import { Model, model, Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+  userID: String,
+  balance: { type: Number, default: 0 },
+});
+
+userSchema.statics.findByUserID = function(userID: string) {
+  return this.findOne({ userID });
+}
+
+interface UserDocument extends Document {
+  userID: string;
+  balance: number;
+};
+
+interface UserModel extends Model<UserDocument> {
+  findByUserID(userID: string): Promise<UserDocument>;
+};
+
+export const User = model<UserDocument>("User", userSchema) as UserModel;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Client } from "discord.js";
 import { CommandManager } from "@jiman24/commandment";
 import path from "path";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { User } from "./database/User";
 
 dotenv.config();
 
@@ -13,6 +15,18 @@ commandManager.verbose = true;
 commandManager.registerCommands(path.resolve(__dirname, "./commands"));
 
 client.on("ready", () => console.log(client.user?.username, "is ready!"))
-client.on("messageCreate", msg => commandManager.handleMessage(msg));
+client.on("messageCreate", async msg => { 
+  
+  const user = await User.findByUserID(msg.author.id);
+
+  if (!user) {
+    const user = new User({ userID: msg.author.id });
+    await user.save();
+  }
+
+  commandManager.handleMessage(msg); 
+});
 
 client.login(process.env.BOT_TOKEN);
+mongoose.connect(process.env.DB_URI!)
+  .then(() => console.log("connected to database"));
