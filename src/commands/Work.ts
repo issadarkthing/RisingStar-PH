f993739df66d5cc42662186cff7aeca7ef86d509
raziman <raziman.terra@gmail.com>
2021-09-08 10:46:08 +0800

    remove magic number

--- src/commands/Work.ts
+++ src/commands/Work.ts
@@ -5,11 +5,13 @@ import { random } from "../structure/utils";
 
 export default class Work extends UserCommand {
   name = "work";
+  min = 1;
+  max = 25;
 
   async exec(msg: Message, args: string[]) {
 
     const user = await this.getUser(msg.author.id);
-    const earned = random().integer(1, 50);
+    const earned = random().integer(this.min, this.max);
 
     user.balance += earned;
     await user.save();
