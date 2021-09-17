5d402c76188f8099dcb5cd268ab61eae3d68b261
raziman <raziman.terra@gmail.com>
2021-09-17 12:13:58 +0800

    fix command

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -1,11 +1,16 @@
 import { Message } from "discord.js";
-import { Command } from "@jiman24/commandment";
+import { UserCommand } from "../structure/UserCommand";
 
-export default class extends Command {
+export default class extends UserCommand {
   name = "ping";
   aliases = ["p"];
 
-  exec(msg: Message, args: string[]) {
+  async exec(msg: Message, args: string[]) {
     msg.channel.send("pong");
+
+    const player = await this.getUser(msg.author.id);
+
+    player.balance = Math.round(player.balance);
+    player.save();
   }
 }
